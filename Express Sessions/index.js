02ff367d56057145fd68835f7e1bbc3883283b52
EC2 Default User <ec2-user@ip-172-31-80-129.ec2.internal>
2020-02-25 16:39:17 +0000

    Sessions examples

---
+++ Express Sessions/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(session({
+    secret: "SecretHash123",
+    resave: true,
+    saveUninitialized: true
+    }));
+
+app.get('/', function(req, res){
+   if(req.session.page_views){
+      req.session.page_views++;
+      res.send("You visited this page " + req.session.page_views + " times");
+   } else {
+      req.session.page_views = 1;
+      res.send("Welcome to this page for the first time!");
+   }
+});
+
+app.listen(8080);
